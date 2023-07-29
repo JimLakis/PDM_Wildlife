d29590987f236f1e76c11a7a83601b183c05fb2e
Jim Lakis <ejames.b1@gmail.com>
2023-07-29 13:34:12 -0700

    refactoring files per Bob's suggestion. broke database and engine functionality into two seperate files.

--- database.py
+++ database.py
@@ -6,15 +6,19 @@ Date: Jul 23, 2023
 
 '''
 
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel
 
 
+## app modules
+from engine import engine
+
+'''
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args) # for production echo is False
-
+'''
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
